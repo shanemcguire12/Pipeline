a9950ca9537563be1ec28a73bde13f9c47ab788a
L00161625 <L00161625@PR2402-21>
2023-12-11 12:30:04 +0000

    firstpush

---
+++ src/MAUITestProject/UnitTest1.cs
@@ -0,0 +1,27 @@
+namespace MAUITestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void DummyTest1()
+        {
+            Assert.True(true);
+
+        }
+
+        [Fact]
+        public void DummyTest2()
+        {
+            Assert.True(true);
+
+        }
+
+        [Fact]
+        public void DummyTest3()
+        {
+            Assert.True(true);
+
+        }
+
+    }
+}
